41748be093e8d9129688017f0b2875f6c64f44c5
Manidoux41 <lherissondu41@gmail.com>
2023-06-17 14:56:16 +0200

    remove export:output

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid"
 
 function Header() {
   return (
@@ -10,6 +11,14 @@ function Header() {
         height={100}
         className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
       />
+      <div>
+        {/* Search Box */}
+        <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+          <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+          <input type="text" placeholder="Search" className="flex-1"/>
+          <button hidden>Search</button>
+        </form>
+      </div>
     </header>
   )
 }
